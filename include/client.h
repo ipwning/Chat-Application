5cd39411b9cc195383d0d687c703e3ef5bb08167
ipwning <ipwn.with@gmail.com>
2022-05-04 19:49:09 +0900

    Edit function definition

--- include/client.h
+++ include/client.h
@@ -3,6 +3,7 @@
 #define SENDER      "Client"
 #define RECEIVER    "Server"
 
-int getPort(const char * argv[]);
+int validateArgv(int argc, const char* argv[]);
 const char* getIP(const char* argv[]);
+int getPort(const char * argv[]);
 int connectSocket(const char* ip, int port);
--- include/server.h
+++ include/server.h
@@ -3,6 +3,5 @@
 #define SENDER      "Server"
 #define RECEIVER    "Client"
 
-void checkArgument(int argc, const char* argv[]);
 int getPort(int argc, const char* argv[]);
 int connectSocket(int port);
