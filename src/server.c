8a2aaaf6dc655d926e0d5cc5f414d003d6f2f426
ipwning <ipwn.with@gmail.com>
2022-05-04 19:40:41 +0900

    Complete to implement server main source (Edit)

--- src/server.c
+++ src/server.c
@@ -4,17 +4,20 @@ int main (int argc, const char* argv[]) {
     int port;
     int fd;
     char *msg;
-    checkArgument(argc, argv);
-    port = getPort(argv[1]);
+    setup();
+    port = getPort(argc, argv);
     fd = connectSocket(port);
 
-    while(true) {
-        sendMsg(fd, SENDER);
-        if(recvMsg(fd) == -1) {
+    while(1) {
+        if(sendMsg(fd, SENDER) == -1) {
+            puts("[Disconnected]");
+            break;
+
+        }
+        if(recvMsg(fd, RECEIVER) == -1) {
             puts("[Disconnected]");
             break;
         }
     }
-    
     return 0;
 }
