63cf77197022e1e2bf86052381ccf82e94c369c7
ipwning <ipwn.with@gmail.com>
2022-05-04 19:37:58 +0900

    Complete to implement common function

--- include/chat.c
+++ include/chat.c
@@ -1,5 +1,12 @@
 #include "../include/chat.h"
 
+
+void setup() {
+    setvbuf(stdin, (char *)NULL, _IONBF, 0);
+    setvbuf(stdout, (char *)NULL, _IONBF, 0);
+    setvbuf(stderr, (char *)NULL, _IONBF, 0);
+}
+
 char* readBuf() {
     char *buf;
     char ch;
@@ -18,27 +25,44 @@ char* readBuf() {
     return buf;
 }
 
-void sendMsg(int fd, const char* sender) {
+int sendMsg(int fd, const char* sender) {
     char *msg = NULL;
     int err;
+    int ret = 0;
     printf("%s: ", sender);
     msg = readBuf();
     assert(send(fd, msg, strlen(msg), 0) != -1);
+    if(strlen(msg) == 5 && !strcmp(msg, "QUIT\n")) {
+        close(fd);
+        ret = -1;
+    }
     free(msg);
+    return ret;
 }
 
 int recvMsg(int fd, const char* receiver) {
     int err;
+    int idx = 0;
+    int ret = 0;
     char ch;
-    char ret = 0;
-    printf("%s: ", receiver);
+    char *msg;
+
+    assert( (msg = calloc(1, sizeof(char) * 0x210) ) != NULL );
     do {
-        assert( (err = recv(fd, (char*)&ch, 1, 0) ) < 0 );  // ERROR
+        assert( (err = recv(fd, (char*)&ch, 1, 0) ) >= 0 );  // ERROR
         if(err == 0) {
             ret = -1;                                       // EOF
             break;
         }
-        putchar(ch); 
+        msg[idx++] = ch;
+        if(idx % 0x200 == 0) { 
+            assert( (msg = realloc(msg, sizeof(char) * (idx + 0x210))) != NULL );
+        }
     } while(ch != '\n');
+    if(!ret) printf("%s: %s", receiver, msg);
+    if(strlen(msg) == 5 && !strcmp(msg, "QUIT\n") ) {
+        ret = -1;
+    }
+    if(ret) close(fd);
     return ret;
 }
--- include/chat.h
+++ include/chat.h
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <assert.h>
+#include <arpa/inet.h>
 #include <sys/types.h>
 #include <sys/socket.h>
-#include <assert.h>
 
+void setup();
 char* readBuf();
-void sendMsg(int fd, const char* sender);
+int sendMsg(int fd, const char* sender);
 int recvMsg(int fd, const char* receiver);
-void checkArgument(int argc, char* argv[]);
